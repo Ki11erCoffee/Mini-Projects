71b827eb897d1cc613b64d20c0d5bfab5faec8fa
JLUXLABEL-Paul <44684944+Ki11erCoffee@users.noreply.github.com>
2021-12-02 14:08:19 -0800

    added comments

--- Vertical-Slider/script.js
+++ Vertical-Slider/script.js
@@ -5,14 +5,18 @@ const upButton = document.querySelector(".up-button");
 const downButton = document.querySelector(".down-button");
 const slidesLength = slideRight.querySelectorAll("div").length;
 
+// Keeps track of which image is displayed
 let activeSlideIndex = 0;
 
+// Orients the last description to display on the screen
 slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
 upButton.addEventListener("click", () => changeSlide("up"));
 downButton.addEventListener("click", () => changeSlide("down"));
 
+// Changes the left and right slide accordingly
 const changeSlide = (direction) => {
+  // Get current display height
   const sliderHeight = sliderContainer.clientHeight;
   if (direction === "up") {
     activeSlideIndex++;
@@ -26,9 +30,11 @@ const changeSlide = (direction) => {
     }
   }
 
+  // Move the right slide up
   slideRight.style.transform = `translateY(-${
     activeSlideIndex * sliderHeight
   }px)`;
+  // Move the left slide down (offset by index * height)
   slideLeft.style.transform = `translateY(${
     activeSlideIndex * sliderHeight
   }px)`;
