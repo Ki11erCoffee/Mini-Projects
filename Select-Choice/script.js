4bcecba5080703397e057695b850c2772bf262d1
JLUXLABEL-Paul <44684944+Ki11erCoffee@users.noreply.github.com>
2021-12-06 07:58:44 -0800

    Update script.js

--- Select-Choice/script.js
+++ Select-Choice/script.js
@@ -1,8 +1,21 @@
-const tagEl = document.getElementById("tags");
+const tagsEl = document.getElementById("tags");
 const textarea = document.getElementById("textarea");
 
 // Automatically have user inside text box
 textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    // Clear input after 10ms
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
+});
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
@@ -17,5 +30,34 @@ function createTags(input) {
     .filter((tag) => tag.trim() !== "")
     .map((tag) => tag.trim());
 
-  console.log(tags);
+  // Clear tags element
+  tagsEl.innerHTML = "";
+
+  // Place create tag element for each element in array
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+  }, 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.rondom() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
