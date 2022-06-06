11c170ff4711a40d999baf20d795e81f86b150bb
Paul Doan <44684944+Ki11erCoffee@users.noreply.github.com>
2022-06-06 15:31:59 -0700

    ajax request

--- ShopifyAjaxProducts/code.js
+++ ShopifyAjaxProducts/code.js
@@ -1,2 +1,8 @@
 history.replaceState(null, '', 'bar5.html');
 // rename the url 
+
+jQuery.getJSON(window.Shopify.routes.root + 'products/orange-new-flame-slinky-skirt-set.js', function(product) {
+  console.log((product));
+} );
+
+// AJAX request in shopify
